cca1709940f0dc2eba36e47b32c920e1971d0040
Martin <mrlabrecque@gmail.com>
2022-12-15 11:14:13 -0500

    Fix server file?

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const express = require("express")
 const app = express()
 
 // use the express-static middleware
-app.use(express.static("public"))
+app.use(express.static(path.join(__dirname, 'dist/foodwithfriends')));
+
 
 // define the first route
-app.get("dist/foodwithfriends", function (req, res) {
-  res.send("<h1>Hello World!</h1>")
-})
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'dist/foodwithfriends/index.html'));
+});
+
 
 // start the server listening for requests
 app.listen(process.env.PORT || 3000, 
